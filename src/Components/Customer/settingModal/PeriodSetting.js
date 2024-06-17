80af2e7d278dbbdf29f88fe6d913ca72fc6ff600
SeungHee <mnb2856@gmail.com>
2024-06-17 09:40:32 +0900

    기간조회 버튼 설정

--- src/Components/Customer/settingModal/PeriodSetting.js
+++ src/Components/Customer/settingModal/PeriodSetting.js
@@ -2,59 +2,78 @@ import * as React from 'react'
 import "../../Main/Main.css"
 
 const SettingModal_Period = () => {
-    const [selectedOption, setSelectedOption] = React.useState('1year');
+    const [selectedOption, setSelectedOption] = React.useState('1년');
+    const [isAbled, setIsAbled] = React.useState(false);
+    const [startDate, setStartDate] = React.useState('');
+    const [endDate, setEndDate] = React.useState('');
 
+    const datas = ['3개월', '6개월', '1년', '사용자 지정']
+
+    React.useEffect(() => {
+        const now = new Date();
+        const today = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString().split('T')[0]; // 오늘 날짜로 설정
+        setEndDate(today);
+
+        const calculateStartDate = (monthsAgo) => {
+            const date = new Date();
+            date.setMonth(date.getMonth() - monthsAgo);
+            return date.toISOString().split('T')[0];
+        };
+
+        switch (selectedOption) {
+            case '3개월':
+                setStartDate(calculateStartDate(3));
+                break;
+            case '6개월':
+                setStartDate(calculateStartDate(6));
+                break;
+            case '1년':
+                setStartDate(calculateStartDate(12));
+                break;
+            case '사용자 지정':
+                setIsAbled(true);
+                setStartDate('');
+                setEndDate(today);
+                break;
+            default:
+                setIsAbled(false);
+                setStartDate('');
+                setEndDate(today);
+                break;
+        }
+    }, [selectedOption]);
     const handleOptionChange = (event) => {
-        setSelectedOption(event.target.value);
+        const value = event.target.value;
+        setSelectedOption(value);
+        setIsAbled(value === '사용자 지정')
+    }
+    const handleStartDateChange = (event) => {
+        setStartDate(event.target.value);
+    }
+    const handleEndDateChange = (event) => {
+        setEndDate(event.target.value);
     }
     return (
         <div>
             <h4>기간선택</h4>
             <div className="customer-status-period-serchbox">
-
                 <div className="radio centered">
-                    <label>
-                        <input
-                            type="radio"
-                            name="search_for"
-                            value="3months"
-                            checked={selectedOption === '3months'}
-                            onChange={handleOptionChange}
-                            onClick={() => { }} />&nbsp;3개월&nbsp;&nbsp;
-                    </label>
-                    <label>
-                        <input
-                            type="radio"
-                            name="search_for"
-                            value="6months"
-                            checked={selectedOption === '6months'}
-                            onChange={handleOptionChange}
-                            onClick={() => { }} />&nbsp;6개월&nbsp;&nbsp;
-                    </label>
-                    <label>
-                        <input
-                            type="radio"
-                            name="search_for"
-                            value="1year"
-                            checked={selectedOption === '1year'}
-                            onChange={handleOptionChange}
-                            onClick={() => { }} />&nbsp;1년&nbsp;&nbsp;
-                    </label>
-                    <label>
-                        <input
-                            type="radio"
-                            name="search_for"
-                            value="custom"
-                            checked={selectedOption === 'custom'}
-                            onChange={handleOptionChange}
-                            onClick={() => { }} />&nbsp;사용자 지정<br />
-                    </label>
-
+                    {datas.map((data) => (
+                        <label key={data}>
+                            <input
+                                type="radio"
+                                name="search_for"
+                                value={data}
+                                checked={selectedOption === data}
+                                onChange={handleOptionChange}
+                            />&nbsp;{data}&nbsp;&nbsp;
+                        </label>
+                    ))}
                 </div>
                 <div className="date centered">
                     {/* 사용자 지정 체크 여부에 따라 활성화 */}
-                    <input type="date" id="startDate" disabled />&nbsp;~&nbsp;
-                    <input type="date" id="endDate" disabled />&nbsp;
+                    <input type="date" id="startDate" disabled={!isAbled} value={startDate} onChange={handleStartDateChange}/>&nbsp;~&nbsp;
+                    <input type="date" id="endDate" disabled={!isAbled} value={endDate} onChange={handleEndDateChange} />&nbsp;
                 </div>
             </div>
             <hr></hr>
--- src/Components/Customer/status_data/PeriodSearch.js
+++ src/Components/Customer/status_data/PeriodSearch.js
@@ -4,23 +4,79 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const CustomerStatusPeriodSerchBox = () => {
+  const [selectedOption, setSelectedOption] = React.useState('1년');
+  const [isAbled, setIsAbled] = React.useState(false);
+  const [startDate, setStartDate] = React.useState('');
+  const [endDate, setEndDate] = React.useState('');
 
+  const datas = ['3개월', '6개월', '1년', '사용자 지정']
+
+  React.useEffect(() => {
+    const now = new Date();
+    const today = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString().split('T')[0]; // 오늘 날짜로 설정
+    setEndDate(today);
+
+    const calculateStartDate = (monthsAgo) => {
+      const date = new Date();
+      date.setMonth(date.getMonth() - monthsAgo);
+      return date.toISOString().split('T')[0];
+    };
+
+    switch (selectedOption) {
+      case '3개월':
+        setStartDate(calculateStartDate(3));
+        break;
+      case '6개월':
+        setStartDate(calculateStartDate(6));
+        break;
+      case '1년':
+        setStartDate(calculateStartDate(12));
+        break;
+      case '사용자 지정':
+        setIsAbled(true);
+        setStartDate('');
+        setEndDate(today);
+        break;
+      default:
+        setIsAbled(false);
+        setStartDate('');
+        setEndDate(today);
+        break;
+    }
+  }, [selectedOption]);
+  const handleOptionChange = (event) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    setIsAbled(value === '사용자 지정')
+  }
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  }
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+  }
   return (
-    <div className="customer-status-period-serchbox"> 
-    
+    <div className="customer-status-period-serchbox">
       <div className="radio righted">
-        <input type="radio" name="search_for" id="3months" onClick={() => { }} />&nbsp;3개월&nbsp;&nbsp;
-        <input type="radio" name="search_for" id="6months" onClick={() => { }} />&nbsp;6개월&nbsp;&nbsp;
-        <input type="radio" name="search_for" id="1year" onClick={() => { }} />&nbsp;1년&nbsp;&nbsp;
-        <input type="radio" name="search_for" id="custom" onClick={() => { }} />&nbsp;사용자 지정<br />
-       
+        {datas.map((data) => (
+          <label key={data}>
+            <input
+              type="radio"
+              name="search_for"
+              value={data}
+              checked={selectedOption === data}
+              onChange={handleOptionChange}
+            />&nbsp;{data}&nbsp;&nbsp;
+          </label>
+        ))}
+
       </div>
       <div className="date righted">
         {/* 사용자 지정 체크 여부에 따라 활성화 */}
-        <input type="date" id="startDate" disabled />&nbsp;~&nbsp;
-        <input type="date" id="endDate" disabled />&nbsp;
+        <input type="date" id="startDate" disabled={!isAbled} value={startDate}  style={{minWidth:'110px'}} onChange={handleStartDateChange}/>&nbsp;~&nbsp;
+        <input type="date" id="endDate" disabled={!isAbled} value={endDate} style={{minWidth:'110px'}} onChange={handleEndDateChange}/>&nbsp;
         <div className="dropdown">
-          <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <button className="btn btn-outline-secondary dropdown-toggle btn-sm " type="button" data-bs-toggle="dropdown" aria-expanded="false">
             검색
           </button>
           <ul className="dropdown-menu">
@@ -33,7 +89,7 @@ const CustomerStatusPeriodSerchBox = () => {
           <span className="input-group-text"><FontAwesomeIcon icon={faSearch} /></span>
           <input type="text" id="keyword" className="form-control" placeholder="검색" />
         </div>&nbsp;
-        <input type="submit" value="조회 >" className="btn btn-dark" />
+        <input type="submit" value="조회" className="btn btn-dark" />
       </div>
     </div>
   );
