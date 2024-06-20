bbf92fd518109972b88adff9ad82897bc264b415
dldmswjd8989 <dldmswjd8989@naver.com>
2024-06-20 14:33:14 +0900

    물류페이지 수정

---
+++ src/Components/Stock/materials/materials.js
@@ -0,0 +1,105 @@
+import * as React from 'react'
+import "../../Main/Main.css"
+import "./materials.css"
+
+const Materials= ()=> {
+    return (
+        <div>
+    
+          <div className="Middle classification">
+            <span> <h2>자재관리</h2> </span>
+          </div>
+    
+          <hr />
+    
+          <div className="top-buttons"> 
+            <span><button>등록</button></span>
+            <span><button>수정</button></span>
+            <span><button>삭제</button></span>
+          </div>
+    
+          <br />
+          
+          <div className="searcher">
+            
+            <div className="left">
+              <div className="middle-buttons">
+              <label for="date">
+                <input type="date" id="date" max="2077-06-20" min="2077-06-05" value="2024-07-18" />
+              </label>
+                  <label>
+                        <select>
+                            <option value="">자재코드</option>
+                            <option value="">품목명선택</option>
+                        </select>
+                  </label>
+              </div>
+            </div>
+            <div className="right">
+              <div className="middle-buttons">
+              <input type="text" placeholder='🔍︎검색' /><button>조회</button>
+              </div>
+              </div>
+
+    
+          </div>
+    
+          <div>
+            <section>
+            <table className='materials-table'>
+                        <thead>
+                            <tr>
+                                <th><input type="checkbox"/></th>
+                                <th>품목코드</th>
+                                <th>품목이름</th>
+                                <th>수량</th>
+                                <th>특이사항</th>
+                                <th>등록일</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><input type="checkbox"/></td>
+                                <td>12345</td>
+                                <td>케이크</td>
+                                <td>1</td>
+                                <td>없음</td>
+                                <td>2024.01.01</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox"/></td>
+                                <td>12345</td>
+                                <td>케이크</td>
+                                <td>1</td>
+                                <td>없음</td>
+                                <td>2024.01.01</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox"/></td>
+                                <td>12345</td>
+                                <td>케이크</td>
+                                <td>1</td>
+                                <td>없음</td>
+                                <td>2024.01.01</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox"/></td>
+                                <td>12345</td>
+                                <td>케이크</td>
+                                <td>1</td>
+                                <td>없음</td>
+                                <td>2024.01.01</td>
+                            </tr>
+                        </tbody>
+                    </table>
+            </section>
+            <div className="bottom-buttons"> 
+            <span><button>엑셀다운</button></span>
+            <span><button>인쇄</button></span>
+          </div>
+          </div>
+        </div>
+    );
+}
+
+export default Materials;
--- src/router/RouterInfo.js
+++ src/router/RouterInfo.js
@@ -8,7 +8,7 @@ import StatusDataMain from '../Components/Customer/status_data/StatusDataMain.js
 
 // 재고 관리
 import StockMgmt from '../Components/Stock/StockMgmt.js';
-import DistributionMgmt from '../Components/Stock/DistributionMgmt.js';
+import DistributionMgmt from '../Components/Stock/distribution/distributionMgmt.js';
 import MeterialMgmt from '../Components/Stock/MeterialMgmt.js';
 import PurchaseMgmt from '../Components/Stock/Purchase/PurchaseMgmt.js';
 import VendorMgmt from '../Components/Stock/Vendor/VendorMgmt.js';
