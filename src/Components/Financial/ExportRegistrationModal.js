fd0ecd21023e7542bc6854c838e7ee7abcf2dbc7
Hyeon Park <phchefc3@gmail.com>
2024-06-24 10:21:17 +0900

    수정
    팝업 및 화면 수정

--- src/Components/Financial/ExportRegistrationModal.js
+++ src/Components/Financial/ExportRegistrationModal.js
@@ -3,14 +3,13 @@ import ModalForm from './ModalForm';
 
 const ExportRegistrationModal = ({ data, setData }) => {
   const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState('');
   const [formData, setFormData] = useState({
     "상품번호": '',
     "상품구분": '',
     "상품명": '',
     "고객명": '',
     "거래일시": '',
-    "결제상태": '완료',
+    "결제상태": '완료', // 기본값 설정
     "금액": '',
     "단가": '',
     "수량": '',
@@ -21,7 +20,6 @@ const ExportRegistrationModal = ({ data, setData }) => {
     const selectedData = data.find(row => row.checked);
     if (selectedData) {
       setFormData(selectedData);
-      setModalContent('거래명세서 작성');
       setShowModal(true);
     } else {
       alert('수정할 항목을 선택해주세요.');
@@ -53,14 +51,10 @@ const ExportRegistrationModal = ({ data, setData }) => {
   };
 
   const handleFormSubmit = () => {
-    if (modalContent === '거래명세서 작성') {
-      setData([...data, { ...formData, checked: false }]);
-    } else if (modalContent === '수정') {
-      const updatedData = data.map((row) =>
-        row.checked ? { ...formData, checked: row.checked } : row
-      );
-      setData(updatedData);
-    }
+    const updatedData = data.map((row) =>
+      row.checked ? { ...formData, checked: row.checked } : row
+    );
+    setData(updatedData);
     handleModalClose();
   };
 
@@ -75,7 +69,7 @@ const ExportRegistrationModal = ({ data, setData }) => {
   return (
     <>
       <div className="righted" style={{ textAlign: 'right', marginTop: '10px' }}>
-        <input type="button" value="거래명세서 작성 >" className="btn btn-dark mr-2" onClick={handleModalShow} />
+        <input type="button" value="거래명세서 수정 >" className="btn btn-dark mr-2" onClick={handleModalShow} />
         <input type="button" value="삭제 >" className="btn btn-dark" onClick={handleDelete} />
       </div>
       <ModalForm
@@ -85,7 +79,7 @@ const ExportRegistrationModal = ({ data, setData }) => {
         handleSubmit={handleFormSubmit}
         formData={formData}
         columns={Object.keys(formData)}
-        modalContent={modalContent}
+        modalContent="수정"
       />
     </>
   );
--- src/Components/Financial/IncomeRegistrationModal.js
+++ src/Components/Financial/IncomeRegistrationModal.js
@@ -3,30 +3,26 @@ import ModalForm from './ModalForm';
 
 const IncomeRegistrationModal = ({ data, setData }) => {
   const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState('');
   const [formData, setFormData] = useState({
     "상품번호": '',
     "상품구분": '',
     "상품명": '',
     "매입처": '',
     "거래일시": '',
-    "결제상태": '완료',
+    "결제상태": '완료', // 기본값 설정
     "금액": '',
     "단가": '',
     "수량": '',
     "특이사항": '',
   });
 
-  const handleModalShow = (content) => {
-    if (content === '거래명세서 작성') {
-      const selectedData = data.find(row => row.checked);
-      if (selectedData) {
-        setFormData(selectedData);
-        setModalContent('거래명세서 작성');
-        setShowModal(true);
-      } else {
-        alert('수정할 항목을 선택해주세요.');
-      }
+  const handleModalShow = () => {
+    const selectedData = data.find(row => row.checked);
+    if (selectedData) {
+      setFormData(selectedData);
+      setShowModal(true);
+    } else {
+      alert('수정할 항목을 선택해주세요.');
     }
   };
 
@@ -55,10 +51,11 @@ const IncomeRegistrationModal = ({ data, setData }) => {
   };
 
   const handleFormSubmit = () => {
-    if (modalContent === '거래명세서 작성') {
-      setData([...data, { ...formData, checked: false }]);
-      handleModalClose();
-    }
+    const updatedData = data.map((row) =>
+      row.checked ? { ...formData, checked: row.checked } : row
+    );
+    setData(updatedData);
+    handleModalClose();
   };
 
   const handleDelete = () => {
@@ -72,7 +69,7 @@ const IncomeRegistrationModal = ({ data, setData }) => {
   return (
     <>
       <div className="righted" style={{ textAlign: 'right', marginTop: '10px' }}>
-        <input type="button" value="거래명세서 작성 >" className="btn btn-dark mr-2" onClick={() => handleModalShow('거래명세서 작성')} />
+        <input type="button" value="거래명세서 수정 >" className="btn btn-dark mr-2" onClick={handleModalShow} />
         <input type="button" value="삭제 >" className="btn btn-dark" onClick={handleDelete} />
       </div>
       <ModalForm
@@ -82,7 +79,7 @@ const IncomeRegistrationModal = ({ data, setData }) => {
         handleSubmit={handleFormSubmit}
         formData={formData}
         columns={Object.keys(formData)}
-        modalContent={modalContent}
+        modalContent="수정"
       />
     </>
   );
