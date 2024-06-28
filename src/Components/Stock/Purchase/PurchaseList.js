845a84c6836101bd53667908cca527d3ac083a6c
zkszka <tenandten10@naver.com>
2024-06-29 02:26:41 +0900

    거래처 검색 완료

--- src/Components/Stock/Purchase/PurchaseList.js
+++ src/Components/Stock/Purchase/PurchaseList.js
@@ -14,6 +14,7 @@ const PurchaseList = ({
   setNewPurchase, setSelectedPurchases, setUpdatePurchase, newPurchase, 
   updatePurchase, isUpdateClicked 
 }) => {
+
   return (
     <div>
       <table className='table'>
